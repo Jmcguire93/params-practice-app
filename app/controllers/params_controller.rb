6dcd4cb34fceb0b0df95d1680046b20b2642a536
James <james.ryan.mcguire@gmail.com>
2021-06-21 10:21:27 -0700

    Adds body_params route path and controller method

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -9,4 +9,9 @@ class ParamsController < ApplicationController
     input_value = params["phrase"].upcase
     render json: { message: "The url segment is #{input_value}." }
   end
+
+  def body_params
+    input_value = params["phrase"].upcase
+    render json: { message: "The body_param is #{input_value}." }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/query_params", controller: "params", action: "query_phrase"
 
   get "/segment_params/:phrase", controller: "params", action: "seg_phrase"
+
+  post "/body_params", controller: "params", action: "body_params"
 end
